c0168c41d48ed646f89ec669f687f892e0498360
Cyril Lefebvre <cyril.lefebvre.27@gmail.com>
2018-10-26 18:08:00 +0200

    feat(component): Agenda with angular-calendar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,9 +9,16 @@ import { ArticleListComponent } from './home/article-list/article-list.component
 import { AgendaComponent } from './home/agenda/agenda.component';
 import { FooterComponent } from './footer/footer.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CalendarModule, DateAdapter } from 'angular-calendar';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
 import { SharedModule } from './shared/shared.module';
 import { ImageCarouselService } from './shared/image-carousel/image-carousel.service';
 import { ArticleService } from './shared/article/article.service';
+
+import { registerLocaleData } from '@angular/common';
+import localeFr from '@angular/common/locales/fr';
+
+registerLocaleData(localeFr);
 
 @NgModule({
   declarations: [
@@ -27,6 +34,10 @@ import { ArticleService } from './shared/article/article.service';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    CalendarModule.forRoot({
+      provide: DateAdapter,
+      useFactory: adapterFactory
+    }),
     SharedModule
   ],
   providers: [
--- src/app/home/agenda/agenda.component.ts
+++ src/app/home/agenda/agenda.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DAYS_OF_WEEK } from 'angular-calendar';
 
 @Component({
   selector: 'lfdj-agenda',
@@ -6,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./agenda.component.scss']
 })
 export class AgendaComponent implements OnInit {
+  actualDate = new Date();
+  weekStartsOn: number = DAYS_OF_WEEK.MONDAY;
+  weekendDays: number[] = [DAYS_OF_WEEK.FRIDAY, DAYS_OF_WEEK.SATURDAY];
+  locale: string;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  changeMonth(isNext: boolean) {
+    this.actualDate.setMonth(isNext ? this.actualDate.getMonth() + 1 : this.actualDate.getMonth() - 1);
+  }
 }
