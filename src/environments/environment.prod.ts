c5449bd4c93833968c41a4d976d0edf77923e8df
Cyril <cyril.lefebvre@soprasteria.com>
2020-02-10 22:58:36 +0100

    feat(Environment) Prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  production: true,
+  apiDomain: 'http://lesfousdujeu.com',
+  apiPath: '/api/',
+  googleAPIURL: 'https://www.googleapis.com/calendar/v3/calendars/',
+  googleAPIKey: 'AIzaSyAQy8kDJKrsyct0g7YnUSbtrXbQN0nLcxQ',
+  googleCalendarID: '5lc6h8ltp9vla0b6llqmj4ss8s@group.calendar.google.com',
+  mailAddress: 'lesfousdujeu@gmail.com',
+  facebookURL: 'https://www.facebook.com/pg/LesFousDuJeu'
 };
