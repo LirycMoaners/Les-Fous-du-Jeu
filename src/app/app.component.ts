46ba45e687baec416ada4d84d41511e7f39e4b47
Cyril <cyril.lefebvre@soprasteria.com>
2020-02-21 15:29:11 +0100

    feat(AppComponent) PWA reload for update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'lesFousDuJeu';
 
-  constructor(public router: Router) {}
+  constructor(
+    public router: Router,
+    private swUpdate: SwUpdate
+  ) {}
+
+  ngOnInit(): void {
+    if (this.swUpdate.isEnabled) {
+      this.swUpdate.available.subscribe(() => {
+        if (confirm('Une nouvelle version est disponible ! Souhaitez-vous recharger votre page pour la visualiser ?')) {
+          window.location.reload();
+        }
+      });
+    }
+  }
 }
